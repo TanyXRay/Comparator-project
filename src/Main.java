cf8377821673f3bd4c97f852439f2abd83b5569f
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-09-23 18:07:37 +0400

    add lambda branch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
  * Решение задачи №1 и №2 по теме: Коллекции TreeSet,TreeMap,Comparator,Comparable.
+ * * Решение задачи №1 и №2 по теме: Лямбда-выражения.
  */
 public class Main {
 
@@ -19,43 +19,17 @@ public class Main {
         people.add(new Person("Костя", "Булдинин-Прохопьев", 21));
         people.add(new Person("Татьяна", "Булкина", 41));
 
-        Collections.sort(people, new ComparatorForPerson(2));
+        Collections.sort(people, (o1, o2) -> {
+            String[] wordsInSurname1 = o1.getSurname().split("\\p{Punct}|\\p{Space}");
+            String[] wordsInSurname2 = o2.getSurname().split("\\p{Punct}|\\p{Space}");
+            int maxLenghtOfSurname = 2;
+            if (wordsInSurname1.length > maxLenghtOfSurname
+                && wordsInSurname2.length > maxLenghtOfSurname) return Integer.compare(o1.getAge(), o2.getAge());
+            if (wordsInSurname1.length > wordsInSurname2.length) return 1;
+            else if (wordsInSurname1.length < wordsInSurname2.length) return -1;
+            else return Integer.compare(o1.getAge(), o2.getAge());
+        });
 
-        for (Person person : people) {
-            System.out.println(person);
-        }
-    }
-}
-
-class ComparatorForPerson implements Comparator<Person> {
-    private int maxLenghtOfSurname;
-
-    public ComparatorForPerson(int maxLenghtOfSurname) {
-        this.maxLenghtOfSurname = maxLenghtOfSurname;
-    }
-
-    /**
-     * Принцип работы метода: у кого больше слов в фамилии, тот более знатен.
-     * Если количество слов в фамилии одинаковы, то знатен больше тот, кто старше.
-     *
-     * @param o1
-     * @param o2
-     * @return
-     */
-    @Override
-    public int compare(Person o1, Person o2) {
-        String[] wordsInSurname1 = o1.getSurname().split("\\p{Punct}|\\p{Space}");
-        String[] wordsInSurname2 = o2.getSurname().split("\\p{Punct}|\\p{Space}");
-        if (wordsInSurname1.length > maxLenghtOfSurname
-            && wordsInSurname2.length > maxLenghtOfSurname) {
-            return Integer.compare(o1.getAge(), o2.getAge());
-        }
-        if (wordsInSurname1.length > wordsInSurname2.length) {
-            return 1;
-        } else if (wordsInSurname1.length < wordsInSurname2.length) {
-            return -1;
-        } else {
-            return Integer.compare(o1.getAge(), o2.getAge());
-        }
+        people.forEach(System.out::println);
     }
 }
